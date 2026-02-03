d5654e215de2f355b9c3f239875370ca2807ea24
mohdthoriq <mohdthoriq@gmail.com>
2026-02-03 13:57:44 +0700

    cleaner

--- App.tsx
+++ App.tsx
@@ -9,21 +9,16 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import ErrorBoundary from './src/components/common/ErrorBoundary';
-import AppNavigator from './src/navigation/AppNavigator';
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <ErrorBoundary>
           <StatusBar 
             backgroundColor='#3a6bbbff' 
             barStyle="light-content" 
             translucent={false}
           />
-          <AppNavigator />
-        </ErrorBoundary>
       </NavigationContainer>
     </SafeAreaProvider>
   );
